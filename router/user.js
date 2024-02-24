0514a5f7eb7984b3aa4f774051c7f6f9b51527ee
Aditya kumar <98655260+164adityakumar@users.noreply.github.com>
2024-02-25 03:39:19 +0530

    Update child_process import and use spawn instead of exec

--- router/user.js
+++ router/user.js
@@ -12,7 +12,7 @@ router.post('/authlogin', login);
 
 router.get('/check-Login',auth, userDetails);
 const  axios  = require('axios');
-const { exec } = require('child_process'); 
+const { spawn } = require('child_process'); 
 const fs = require('fs');
 
 router.post('/authlogin', login); 
@@ -31,19 +31,28 @@ async function downloadFile(url, filePath) {
   }
   
 
-
 router.post('/get-file-url', uploadMiddleWare , uploadFile , async(req,res) => {
-    const s3Url = req.files[0].location;
-    const inputFilePath = `.\\input-videos\\input.${req.files[0].originalname.split('.').pop()}`;
-    await downloadFile(s3Url, inputFilePath);
+  const s3Url = req.files[0].location;
+  // const inputFilePath = ".\\input-videos\\input."+ req.files[0].originalname.split('.').pop();
+  const inputFilePath = "./input-videos/input."+ req.files[0].originalname.split('.').pop();
+
+  await downloadFile(s3Url, inputFilePath);
   try {
     const clamAVScan = await new Promise((resolve, reject) => {
       console.log("Initiating ClamAV scan...");
-      exec(`clamscan ${inputFilePath}`, (error, stdout, stderr) => {
-        if (error) {
-          console.error("Error executing ClamAV command:", error);
-          reject(error);
-        }
+      const clamscan = spawn('clamscan', [inputFilePath]);
+
+      let stdout = '';
+      clamscan.stdout.on('data', (data) => {
+        stdout += data;
+      });
+
+      clamscan.on('error', (error) => {
+        console.error("Error executing ClamAV command:", error);
+        reject(error);
+      });
+
+      clamscan.on('close', (code) => {
         resolve(stdout);
       });
     });
@@ -51,18 +60,18 @@ router.post('/get-file-url', uploadMiddleWare , uploadFile , async(req,res) => {
     console.log("ClamAV scan result:", clamAVScan);
 
     if (clamAVScan.includes("Infected files: 0")) {
-      console.log("File is clean. Proceeding with transcoding...");
+      console.log("File is clean");
       return res.status(200).json({
         clamAVScan,
         success: true, 
         message: 'File is clean.', 
-        })
+      })
     }
     return res.status(200).json({
       clamAVScan,
       success: false, 
       message: 'Image was successfully uploaded', 
-      })
+    })
 
   } catch (error) {
     console.error("Error during ClamAV scan:", error);
@@ -70,7 +79,6 @@ router.post('/get-file-url', uploadMiddleWare , uploadFile , async(req,res) => {
       error: "Error during ClamAV scan",
     });
   }
-
 });
 
 
