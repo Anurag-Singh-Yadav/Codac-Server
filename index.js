ee570f660386877ef05c6e1920165793c5b730d6
aditya-kumar-25 <aditya.pca.25@gmail.com>
2024-02-24 18:47:43 +0530

    back

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 
 app.use('/codac', require('./router/user'));
 
-const PORT = 4041 ;
+const PORT = 4000 ;
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
--- router/user.js
+++ router/user.js
@@ -2,17 +2,18 @@ const express = require('express');
 const router = express.Router();
 // constroller 
 const { login } = require('../controllers/autoAuth');
-const { uploadMiddleWare } = require('../middleware/upload');
-const {uploadFile} = require('../controllers/upload');
+const uploadMiddleWare = require('../middleware/upload');
+const { uploadFile } = require('../controllers/upload');
 router.post('/authlogin', login); 
 
-router.post('/get-file-url',uploadMiddleWare  , uploadFile , (req,res) => {
+router.post('/get-file-url', uploadMiddleWare , uploadFile , (req,res) => {
     return res.status(200).json({
         location: req.files[0].location,
-        success: true,
+        success: true, 
         message: 'Image was successfully uploaded',
     })
-})
+});
 
 module.exports = router;
 
+ 
