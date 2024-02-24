ed7919d5caaab98146859a89f9b36baafef93276
aditya-kumar-25 <aditya.pca.25@gmail.com>
2024-02-25 04:51:52 +0530

    kk

--- router/user.js
+++ router/user.js
@@ -1,62 +1,60 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-// constroller 
-const { login } = require('../controllers/autoAuth');
-const uploadMiddleWare = require('../middleware/upload');
-const { uploadFile } = require('../controllers/upload');
-const { auth } = require('../middleware/auth');
-const {userDetails} = require('../controllers/userDetails');
-const {createUser, manualLogin} = require('../controllers/manualLogin');
+// constroller
+const { login } = require("../controllers/autoAuth");
+const uploadMiddleWare = require("../middleware/upload");
+const { uploadFile } = require("../controllers/upload");
+const { auth } = require("../middleware/auth");
+const { userDetails } = require("../controllers/userDetails");
+const { createUser, manualLogin } = require("../controllers/manualLogin");
+const xlsx = require("xlsx");
 
-router.post('/create-user',createUser);
+router.post("/create-user", createUser);
 
-router.post('/manualLogin',manualLogin);
+router.post("/manualLogin", manualLogin);
 
-router.post('/authlogin', login);
+router.post("/authlogin", login);
 
-router.get('/check-Login',auth, userDetails);
-const  axios  = require('axios');
-const { spawn } = require('child_process'); 
-const fs = require('fs');
+router.get("/check-Login", auth, userDetails);
+const axios = require("axios");
+const { spawn } = require("child_process");
+const fs = require("fs");
 
-router.post('/authlogin', login); 
-
-
+router.post("/authlogin", login);
 
 async function downloadFile(url, filePath) {
-    try {
-
-      const response = await axios.get(url, { responseType: "arraybuffer" });
-      fs.writeFileSync(filePath, response.data);
-      console.log(`Downloaded and saved file to ${filePath}`);
-    } catch (error) {
-      console.error("Error downloading file:", error);
-    }
+  try {
+    const response = await axios.get(url, { responseType: "arraybuffer" });
+    fs.writeFileSync(filePath, response.data);
+    console.log(`Downloaded and saved file to ${filePath}`);
+  } catch (error) {
+    console.error("Error downloading file:", error);
   }
-  
+}
 
-router.post('/get-file-url', uploadMiddleWare , uploadFile , async(req,res) => {
+router.post("/get-file-url", uploadMiddleWare, uploadFile, async (req, res) => {
   const s3Url = req.files[0].location;
   // const inputFilePath = ".\\input-videos\\input."+ req.files[0].originalname.split('.').pop();
-  const inputFilePath = "./input-videos/input."+ req.files[0].originalname.split('.').pop();
+  const inputFilePath =
+    "./input-videos/input." + req.files[0].originalname.split(".").pop();
 
   await downloadFile(s3Url, inputFilePath);
   try {
     const clamAVScan = await new Promise((resolve, reject) => {
       console.log("Initiating ClamAV scan...");
-      const clamscan = spawn('clamscan', [inputFilePath]);
+      const clamscan = spawn("clamscan", [inputFilePath]);
 
-      let stdout = '';
-      clamscan.stdout.on('data', (data) => {
+      let stdout = "";
+      clamscan.stdout.on("data", (data) => {
         stdout += data;
       });
 
-      clamscan.on('error', (error) => {
+      clamscan.on("error", (error) => {
         console.error("Error executing ClamAV command:", error);
         reject(error);
       });
 
-      clamscan.on('close', (code) => {
+      clamscan.on("close", (code) => {
         resolve(stdout);
       });
     });
@@ -67,16 +65,15 @@ router.post('/get-file-url', uploadMiddleWare , uploadFile , async(req,res) => {
       console.log("File is clean");
       return res.status(200).json({
         clamAVScan,
-        success: true, 
-        message: 'File is clean.', 
-      })
+        success: true,
+        message: "File is clean.",
+      });
     }
     return res.status(200).json({
       clamAVScan,
-      success: false, 
-      message: 'Image was successfully uploaded', 
-    })
-
+      success: false,
+      message: "Image was successfully uploaded",
+    });
   } catch (error) {
     console.error("Error during ClamAV scan:", error);
     return res.status(500).json({
@@ -85,9 +82,34 @@ router.post('/get-file-url', uploadMiddleWare , uploadFile , async(req,res) => {
   }
 });
 
+router.post("/preview", uploadMiddleWare, uploadFile, async (req, res) => {
+  const s3Url = req.files[0].location;
+  const inputFilePath =
+    "./input-videos/input." + req.files[0].originalname.split(".").pop();
 
-
+  await downloadFile(s3Url, inputFilePath);
+  try {
+    const workbook = await xlsx.readFile(inputFilePath);
+    const sheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[sheetName];
+    const previewRows = 5;
+    let preview = "";
+    for (let row = 1; row <= previewRows; row++) {
+      let rowData = [];
+      for (let col in worksheet) {
+        if (worksheet[col][row]) {
+          rowData.push(worksheet[col][row].v);
+        } else {
+          rowData.push("");
+        }
+      }
+      preview += rowData.join(" ") + "\n";
+    }
+    res.status(200).send({ preview });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send({ message: "Error generating preview" });
+  }
+});
 
 module.exports = router;
-
- 
